5fb3d0b86842de69962c600d011d732557c57146
Christopher Amanor <kriskit7@gmail.com>
2020-06-22 09:20:50 +0000

    Add solution to palindrome index challenge on HackerRank

---
+++ palindrome_index.rb
@@ -0,0 +1,18 @@
+def palindromeIndex(s)
+  left_index = 0
+  right_index = s.size - 1
+
+  while left_index < right_index && (s[left_index] == s[right_index])
+    left_index += 1
+    right_index -= 1
+  end
+
+  return left_index if palindrome?(s[0...left_index] + s[(left_index + 1)...s.size])
+  return right_index if palindrome?(s[0...right_index] + s[(right_index + 1)...s.size])
+  return -1
+
+end
+
+def palindrome?(s)
+  s == s.reverse
+end
