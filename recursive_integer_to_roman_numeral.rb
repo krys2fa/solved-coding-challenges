28073669f2d26d145f165d65d7eec52f661e8d36
Christopher Amanor <kriskit7@gmail.com>
2020-10-01 21:47:24 +0000

    Add solution to recursive integer to roman numeral assignment

---
+++ recursive_integer_to_roman_numeral.rb
@@ -0,0 +1,8 @@
+def integer_to_roman(roman_mapping, number, result = "")
+  return result if number == 0
+  roman_mapping.keys.each do |divisor|
+    quotient, modulus = number.divmod(divisor)
+    result << roman_mapping[divisor] * quotient
+    return integer_to_roman(roman_mapping, modulus, result) if quotient > 0
+  end
+end
