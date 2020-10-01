77ad6234d897ed529b8a6367518838f407a836c9
Christopher Amanor <kriskit7@gmail.com>
2020-10-01 21:48:39 +0000

    Add solution to recursive roman numeral to integer assignment

---
+++ recursive_roman_numeral_to_integer.rb
@@ -0,0 +1,10 @@
+def roman_to_integer(roman_mapping, str, result = 0)
+  return result if str.empty?
+  roman_mapping.keys.each do |roman|
+    if str.start_with?(roman)
+      result += roman_mapping[roman]
+      str = str.slice(roman.length, str.length)
+      return roman_to_integer(roman_mapping, str, result)
+    end
+  end
+end
