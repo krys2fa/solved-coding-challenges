3b648509109a55a508c5d85bbd1a2c6f2b0b52db
Christopher Amanor <kriskit7@gmail.com>
2020-11-19 16:03:54 +0000

    Update Quick Sort I challenge with Model solution

--- sorting_algorithms/quick_sort_1_replit.rb
+++ sorting_algorithms/quick_sort_1_replit.rb
@@ -14,3 +14,22 @@ end
 
 p partition([4, 5, 3, 9, 1])
 # => [3, 1, 4, 5, 9]
+
+# Model Solution
+
+def partition(array)
+  # write your code here
+  pivot = array.first
+  smaller = []
+  greater = []
+
+  array[1..-1].each do |number|
+    if number > pivot
+      greater << number
+    else
+      smaller << number
+    end
+  end
+
+  smaller + [pivot] + greater
+end
