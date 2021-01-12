c78ad1cc7dcdc51263a2f1cee373ad8636efb5d5
Christopher Amanor <kriskit7@gmail.com>
2021-01-12 10:15:42 +0000

    Add method to ensure original array is not modified

--- hackerrank_challenges/contains_duplicates.rb
+++ hackerrank_challenges/contains_duplicates.rb
@@ -1,7 +1,7 @@
 # @param {Integer[]} nums
 # @return {Boolean}
 def contains_duplicate(nums)
-  sorted_nums = nums.sort
+  sorted_nums = nums.dup.sort
   sorted_nums.each_cons(2) do |x, y|
     return true if x == y
   end
