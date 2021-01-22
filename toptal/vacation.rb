bbd6ee4b789d7b760dc2b3cb72692870abf12c3e
Christopher Amanor <kriskit7@gmail.com>
2021-01-22 21:01:47 +0000

    Amend solution to string toptal challenge

--- toptal/vacation.rb
+++ toptal/vacation.rb
@@ -1,3 +1,5 @@
+require 'Date'
+
 def solution(y, a, b, w)
   leap = true if y % 4 == 0
 
@@ -15,14 +17,32 @@ def solution(y, a, b, w)
    'November' => 30,
    'December' => 31
   }
+
+  days = {
+    'Monday' => 1,
+    'Tuesday' => 2,
+    'Wednesday' => 2,
+    'Thursday' => 3,
+    'Friday' => 4,
+    'Saturday' => 5,
+    'Sunday' => 0,
+  }
   first_month = months[a]
   last_month = months[b]
 
   total_days = first_month + last_month
+  day_of_week = days[w]
+  # puts day_of_week
 
-  if w != 'Monday'
-    return ((total_days - 7) / 7).floor
+   puts Date.new(2014, 1, 1).wday
+
+  if day_of_week != 0
+  # puts ((total_days - 7) / 7).floor
+    # puts ((total_days - 7) / 7).floor
   else
-    return (total_days / 7).floor
+  puts false
+    puts (total_days / 7).floor
   end
 end
+
+solution(2014, 'May', 'June', 'Wednesday')
