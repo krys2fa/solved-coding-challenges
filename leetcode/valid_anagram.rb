8f28a187f922c8fd70110712f647ded871cc6138
Christopher Amanor <kriskit7@gmail.com>
2021-01-29 23:05:41 +0000

    Add solution to Valid Anagram challenge on Leetcode

---
+++ leetcode/valid_anagram.rb
@@ -0,0 +1,26 @@
+# @param {String} s
+# @param {String} t
+# @return {Boolean}
+def is_anagram(s, t)
+  return false if s.length != t.length
+
+  s_arr = s.split('')
+  t_arr = t.split('')
+
+  hash = Hash.new
+
+  s_arr.each do |ele|
+    hash[ele] = hash.has_key?(ele) ? hash[ele] += 1 : 1
+  end
+
+  t_arr.each do |ele|
+    hash[ele] = hash.has_key?(ele) ? hash[ele] -= 1 : 1
+  end
+
+  hash.each do |key, val|
+    return false if val != 0
+  end
+
+  return true
+
+end
